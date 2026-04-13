d1ff53f4d846665535f832c4c0c20639f3c2a6c8
Renan Santos Mendes <renansantosmendes@gmail.com>
2026-04-13 10:04:51 -0300

    fix package version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pgl-utils",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="your.email@example.com",
     description="Machine Learning, Deep Learning, and GenAI utilities for Post-Graduation Lectures",
