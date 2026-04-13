e4803bd4e8b71cc11ae310d4075552553ecce59d
Renan Santos Mendes <renansantosmendes@gmail.com>
2026-04-13 00:32:34 -0300

    remove black

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/renansantosmendes/pgl_utils",
-    packages=find_packages(),
+    packages=find_packages(exclude=["black"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
