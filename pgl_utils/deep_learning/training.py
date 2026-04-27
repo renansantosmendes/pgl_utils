a4698ec1798a87f84be77509999f3358c361e13c
Renan Santos Mendes <renansantosmendes@gmail.com>
2026-04-27 00:54:25 -0300

    fix default seed

--- pgl_utils/deep_learning/training.py
+++ pgl_utils/deep_learning/training.py
@@ -1,10 +1,35 @@
-"""
-Deep Learning training utilities
-"""
+import os
 
 
-def placeholder():
+def set_reprodutibility(seed: int = 42) -> None:
     """
-    Placeholder function
+    Configure global determinism for reproducibility across Python, NumPy,
+    and backend engine.
+
+    Args:
+        seed (int): Seed value used to control randomness.
     """
-    return "DL training utilities"
+
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    os.environ["TF_DETERMINISTIC_OPS"] = "1"
+    os.environ["TF_CUDNN_DETERMINISTIC"] = "1"
+
+    import random
+    import numpy as np
+
+    random.seed(seed)
+    np.random.seed(seed)
+
+    import tensorflow as tf
+
+    tf.random.set_seed(seed)
+
+    try:
+        tf.config.threading.set_inter_op_parallelism_threads(1)
+        tf.config.threading.set_intra_op_parallelism_threads(1)
+        print("Determinismo configurado com sucesso.")
+    except RuntimeError as e:
+        print(f"Erro ao configurar threads: {e}")
+        print(
+            "DICA: Reinicie o kernel e execute esta função como a primeira operação do TensorFlow."
+        )
